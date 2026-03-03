edd84232968fd0dab3019dd63bf051a018c18a73
ManuelSerna <manuelserna1997@gmail.com>
2026-03-03 15:51:53 -0600

    Pushing visualization of gridding the tobacco product images.

---
+++ pilot_work/patchify/patch.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from nano_vectordb import NanoVectorDB
+import numpy as np
+import pandas as pd
+
+
+def main():
+    test_img_df = pd.read_csv("/home/mserna/projects/tobacco-projects/smart_connect_health_neurips_2026/data/tobacco_1m_miccai/test_img_df.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ pilot_work/readme.md
@@ -0,0 +1,1 @@
+This pilot work directory is for testing out ideas in isolation before bringing them out to the full project outside this dir.
